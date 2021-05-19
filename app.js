91eb7e3a073f68300ad35de441c81e2353ac869c
Leon0241 <enliang.leon.lee@gmail.com>
2021-05-20 00:53:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-   //send file index.html to user
-   req.sendFile('index.html');
- });
+// app.get('/', (req, res) => {
+//    //send file index.html to user
+//    req.sendFile('index.html');
+//  });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
