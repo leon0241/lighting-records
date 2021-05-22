15eb8d6388185e8ae9c7a26e40ea7a2bd35ef158
Leon0241 <enliang.leon.lee@gmail.com>
2021-05-22 03:08:05 +0100

    doing some firebase stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,23 @@
-let addButton = document.getElementById("addAnotherRecord")
-let group = document.getElementById("recordData1")
-let container = document.getElementById("addRecords")
-
+let addButton = document.getElementById("addAnotherRecord");
+let group = document.getElementById("recordData1");
+let container = document.getElementById("addRecords");
 addButton.addEventListener("click", (e) => {
-  let newGroup = group.cloneNode(true)
-  container.appendChild(newGroup)
-  container.scrollTop = container.scrollHeight;
+    let newGroup = group.cloneNode(true);
+    container.appendChild(newGroup);
+    container.scrollTop = container.scrollHeight;
+});
+
+let db = firebase.firestore();
+
+db.collection("artists").add({
+    band: true,
+    genres: ["rock"],
+    name: "linkin park"
 })
+
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
